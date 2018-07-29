3a5891c3f2affd7404a36f9b2a66bb4bdb0460a4
Emily. C <kauket19@gmail.com>
2018-07-29 17:07:13 +1000

    Revert "ReturnBookUi - Clean up"
    This reverts commit a939a5f

--- src/ReturnBookUI.java
+++ src/ReturnBookUI.java
@@ -2,83 +2,86 @@ import java.util.Scanner;
 
 
 public class ReturnBookUI {
-    
-    public enum UI_STATE {INITIALISED, READY, INSPECTING, COMPLETED}
-    
-    private ReturnBookControl returnBookControl;
-    private Scanner userInput;
-    private UI_STATE uiState;
-    
-    
-    public ReturnBookUI(ReturnBookControl returnBookControl) {
-        this.returnBookControl = returnBookControl;
-        userInput = new Scanner(System.in);
-        uiState = UI_STATE.INITIALISED;
-        returnBookControl.setUI(this);
-    }
-    
-    
-    public void run() {
-        output("Return Book Use Case UI\n");
-        
-        while (true) {
-            
-            switch (uiState) {
-                
-                case INITIALISED:
-                    break;
-                
-                case READY:
-                    String bookIdString = input("Scan Book (<enter> completes): ");
-                    if (bookIdString.length() == 0) {
-                        returnBookControl.scanningComplete();
-                    } else {
-                        try {
-                            int bookId = Integer.valueOf(bookIdString).intValue();
-                            returnBookControl.bookScanned(bookId);
-                        } catch (NumberFormatException e) {
-                            output("Invalid bookId");
-                        }
-                    }
-                    break;
-                
-                case INSPECTING:
-                    String answer = input("Is book damaged? (Y/N): ");
-                    boolean isDamaged = false;
-                    if (answer.toUpperCase().equals("Y")) {
-                        isDamaged = true;
-                    }
-                    returnBookControl.dischargeLoan(isDamaged);
-                
-                case COMPLETED:
-                    output("Return processing complete");
-                    return;
-                
-                default:
-                    output("Unhandled uiState");
-                    throw new RuntimeException("ReturnBookUI : unhandled uiState :" + uiState);
-            }
-        }
-    }
-    
-    
-    private String input(String prompt) {
-        System.out.print(prompt);
-        return userInput.nextLine();
-    }
-    
-    
-    private void output(Object object) {
-        System.out.println(object);
-    }
-    
-    
-    public void display(Object object) {
-        output(object);
-    }
-    
-    
-    public void setState(UI_STATE uiState) {
-        this.uiState = uiState;
-    }
+
+	public static enum UI_STATE { INITIALISED, READY, INSPECTING, COMPLETED };
+
+	private ReturnBookControl control;
+	private Scanner input;
+	private UI_STATE state;
+
+	
+	public ReturnBookUI(ReturnBookControl control) {
+		this.control = control;
+		input = new Scanner(System.in);
+		state = UI_STATE.INITIALISED;
+		control.setUI(this);
+	}
+
+
+	public void run() {		
+		output("Return Book Use Case UI\n");
+		
+		while (true) {
+			
+			switch (state) {
+			
+			case INITIALISED:
+				break;
+				
+			case READY:
+				String bookStr = input("Scan Book (<enter> completes): ");
+				if (bookStr.length() == 0) {
+					control.scanningComplete();
+				}
+				else {
+					try {
+						int bookId = Integer.valueOf(bookStr).intValue();
+						control.bookScanned(bookId);
+					}
+					catch (NumberFormatException e) {
+						output("Invalid bookId");
+					}					
+				}
+				break;				
+				
+			case INSPECTING:
+				String ans = input("Is book damaged? (Y/N): ");
+				boolean isDamaged = false;
+				if (ans.toUpperCase().equals("Y")) {					
+					isDamaged = true;
+				}
+				control.dischargeLoan(isDamaged);
+			
+			case COMPLETED:
+				output("Return processing complete");
+				return;
+			
+			default:
+				output("Unhandled state");
+				throw new RuntimeException("ReturnBookUI : unhandled state :" + state);			
+			}
+		}
+	}
+
+	
+	private String input(String prompt) {
+		System.out.print(prompt);
+		return input.nextLine();
+	}	
+		
+		
+	private void output(Object object) {
+		System.out.println(object);
+	}
+	
+			
+	public void display(Object object) {
+		output(object);
+	}
+	
+	public void setState(UI_STATE state) {
+		this.state = state;
+	}
+
+	
 }
