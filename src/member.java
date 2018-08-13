90c48751c498bcf29b0abc42975ee0ec28dca21f
MethodicalMadness <lucifersbeard@hotmail.com>
2018-08-13 20:17:02 +1000

    replacesd tabs with spaces. adjusted formatting. renamed some variables.

--- src/member.java
+++ src/member.java
@@ -4,114 +4,110 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
 @SuppressWarnings("serial")
 public class member implements Serializable {
 
-	private String LN;
-	private String FN;
-	private String EM;
-	private int PN;
-	private int ID;
-	private double FINES;
-	
-	private Map<Integer, Loan> LNS;
 
-	
-	public member(String lastName, String firstName, String email, int phoneNo, int id) {
-		this.LN = lastName;
-		this.FN = firstName;
-		this.EM = email;
-		this.PN = phoneNo;
-		this.ID = id;
-		
-		this.LNS = new HashMap<>();
-	}
-
-	
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("getMember:  ").append(ID).append("\n")
-		  .append("  Name:  ").append(LN).append(", ").append(FN).append("\n")
-		  .append("  Email: ").append(EM).append("\n")
-		  .append("  Phone: ").append(PN)
-		  .append("\n")
-		  .append(String.format("  Fines Owed :  $%.2f", FINES))
-		  .append("\n");
-		
-		for (Loan loan : LNS.values()) {
-			sb.append(loan).append("\n");
-		}		  
-		return sb.toString();
-	}
-
-	
-	public int getId() {
-		return ID;
-	}
-
-	
-	public List<Loan> getLoans() {
-		return new ArrayList<Loan>(LNS.values());
-	}
-
-	
-	public int getNumberOfCurrentLoans() {
-		return LNS.size();
-	}
-
-	
-	public double getFinesOwed() {
-		return FINES;
-	}
-
-	
-	public void takeOutLoan(Loan loan) {
-		if (!LNS.containsKey(loan.getId())) {
-			LNS.put(loan.getId(), loan);
-		}
-		else {
-			throw new RuntimeException("Duplicate loan added to member");
-		}		
-	}
-
-	
-	public String getLastName() {
-		return LN;
-	}
-
-	
-	public String getFirstName() {
-		return FN;
-	}
+    private String lastName;
+    private String firstName;
+    private String email;
+    private int phoneNumber;
+    private int id;
+    private double fines;
+    private Map<Integer, Loan> loanMap;
 
 
-	public void addFine(double fine) {
-		FINES += fine;
-	}
-	
-	public double payFine(double amount) {
-		if (amount < 0) {
-			throw new RuntimeException("getMember.payFine: amount must be positive");
-		}
-		double change = 0;
-		if (amount > FINES) {
-			change = amount - FINES;
-			FINES = 0;
-		}
-		else {
-			FINES -= amount;
-		}
-		return change;
-	}
+    public member(String lastName, String firstName, String email, int phoneNumber, int id) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.id = id;
+        this.loanMap = new HashMap<>();
+    }
 
 
-	public void dischargeLoan(Loan loan) {
-		if (LNS.containsKey(loan.getId())) {
-			LNS.remove(loan.getId());
-		}
-		else {
-			throw new RuntimeException("No such loan held by member");
-		}		
-	}
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("getMember:  ").append(id).append("\n")
+          .append("  Name:  ").append(lastName).append(", ").append(firstName).append("\n")
+          .append("  Email: ").append(email).append("\n")
+          .append("  Phone: ").append(phoneNumber)
+          .append("\n")
+          .append(String.format("  Fines Owed :  $%.2f", fines))
+          .append("\n");
+        for (Loan loan : loanMap.values()) {
+            sb.append(loan).append("\n");
+        }
+        return sb.toString();
+    }
 
+
+    public int getId() {
+        return id;
+    }
+
+
+    public List<Loan> getLoans() {
+        return new ArrayList<Loan>(loanMap.values());
+    }
+
+
+    public int getNumberOfCurrentLoans() {
+        return loanMap.size();
+    }
+
+
+    public double getFinesOwed() {
+        return fines;
+    }
+
+
+    public void takeOutLoan(Loan loan) {
+        if (!loanMap.containsKey(loan.getId())) {
+            loanMap.put(loan.getId(), loan);
+        } else {
+            throw new RuntimeException("Duplicate loan added to member");
+        }
+    }
+
+
+    public String getLastName() {
+        return lastName;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+
+    public void addFine(double fine) {
+        fines += fine;
+    }
+
+
+    public double payFine(double amount) {
+        if (amount < 0) {
+            throw new RuntimeException("getMember.payFine: amount must be positive");
+        }
+        double change = 0;
+        if (amount > fines) {
+            change = amount - fines;
+            fines = 0;
+        } else {
+            fines -= amount;
+        }
+        return change;
+    }
+
+
+    public void dischargeLoan(Loan loan) {
+        if (loanMap.containsKey(loan.getId())) {
+            loanMap.remove(loan.getId());
+        } else {
+            throw new RuntimeException("No such loan held by member");
+        }
+    }
 }
